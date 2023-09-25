c0504a9dff5c7e800792032d04dee51b1b66cc1c
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-25 16:30:29 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers using bubble sort algorithm
+ * @array: the array to be sorted
+ * @size: the number of elements in the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, swapped;
+
+	if (array == NULL || size < 2)
+		return;
+	
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+
+		/* if no two elements were swapped in inner loop, array is sorted*/
+		if (swapped == 0)
+			break;
+	}
+}
