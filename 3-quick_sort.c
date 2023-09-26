d8153415cde58bd729ed815cd1a5b7bf605cc0e6
Onyedikachi Eze <thekacchi1@gmail.com>
2023-09-25 19:57:10 -0700

    task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,72 @@
+#include "sort.h"
+
+/** 
+ * lomuto_partition - implements the lomuto partition scheme
+ *
+ * @array: the array to ge partitioned
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: The size of the array
+ *
+ * Return: The partition index
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j, temp;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			print_array(array, size);
+		}
+	}
+
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	print_array(array, size);
+
+	return (i + 1);
+}
+
+/**
+ * quicksort - Recursively sorts an array using wuick sort
+ *
+ * @array: the array to be sorted
+ * @low: the low index of the partition
+ * @high: the high index of the partition
+ * @size: the size of the array
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int pi;
+
+	if (low < high)
+	{
+		pi = lomuto_partition(array, low, high, size);
+
+		quicksort(array, low, pi - 1, size);
+		quicksort(array, pi + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers ising Quick Sort (LP)
+ * 
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort(array, 0, (int)size - 1, size);
+}
